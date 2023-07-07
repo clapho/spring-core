95e0931dfbd1cee88a7596ceecfcbabe2e27b218
SoohoPark <clapp301@gmail.com>
2023-07-07 11:44:47 +0900

    주문과 할인 도메인 개발 및 테스트

---
+++ src/test/java/com/example/springcore/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.springcore.order;
+
+import com.example.springcore.member.Grade;
+import com.example.springcore.member.Member;
+import com.example.springcore.member.MemberService;
+import com.example.springcore.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
