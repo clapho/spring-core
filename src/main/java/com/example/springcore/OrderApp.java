b33aad8743eaf42d1002765e8d2401de40e00fce
SoohoPark <clapp301@gmail.com>
2023-07-07 17:54:37 +0900

    스프링 전환

--- src/main/java/com/example/springcore/OrderApp.java
+++ src/main/java/com/example/springcore/OrderApp.java
@@ -6,19 +6,26 @@ import com.example.springcore.member.Member;
 import com.example.springcore.member.MemberService;
 import com.example.springcore.order.Order;
 import com.example.springcore.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         System.out.println("order = " + order);
     }
