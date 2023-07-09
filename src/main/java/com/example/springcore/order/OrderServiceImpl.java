468c47c7979416b66bb59f5c3e2e438be50a7be9
SoohoPark <clapp301@gmail.com>
2023-07-09 18:55:49 +0900

    컴포넌트 스캔

--- src/main/java/com/example/springcore/order/OrderServiceImpl.java
+++ src/main/java/com/example/springcore/order/OrderServiceImpl.java
@@ -1,16 +1,18 @@
 package com.example.springcore.order;
 
 import com.example.springcore.discount.DiscountPolicy;
-import com.example.springcore.discount.FixDiscountPolicy;
 import com.example.springcore.member.Member;
 import com.example.springcore.member.MemberRepository;
-import com.example.springcore.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
